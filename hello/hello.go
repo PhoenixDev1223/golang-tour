37067dad98520d88d1ff264f03a5aba44d0e21ba
PhoenixDev1223 <phoenixdev1223@gmail.com>
2022-08-18 10:46:22 +0900

    Return greetings for multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -11,11 +11,14 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Dolly")
+	names := []string{"Rossi", "Dolly", "Tom"}
+	messages, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	for _, message := range messages {
+		fmt.Println(message)
+	}
 }
